cfd29b67b40be644fb7a314ee310912de76f0f98
ianzapolsky <ianzapolsky@gmail.com>
2015-02-13 20:22:48 +0000

    updated replace.sh

--- replace.sh
+++ replace.sh
@@ -1,5 +1,7 @@
 #! /bin/sh
 
+cd /home/ubuntu/alumnus-django/
+git pull --rebase origin master
 sudo docker build -t alumnus .
 sudo docker rm -f $(sudo docker ps -a -q)
 sudo docker run -d -p 127.0.0.1:8000:8000 alumnus
